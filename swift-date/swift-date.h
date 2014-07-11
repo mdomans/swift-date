512a9b9baa09b41e708f27555fcae91c3b067a2c
mdomans <michaldoman@k2.pl>
2014-07-11 14:07:46 +0200

    Initial Commit

---
+++ swift-date/swift-date.h
@@ -0,0 +1,19 @@
+//
+//  swift-date.h
+//  swift-date
+//
+//  Created by mdomans on 11/07/14.
+//  Copyright (c) 2014 mdomans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift-date.
+FOUNDATION_EXPORT double swift_dateVersionNumber;
+
+//! Project version string for swift-date.
+FOUNDATION_EXPORT const unsigned char swift_dateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_date/PublicHeader.h>
+
+
